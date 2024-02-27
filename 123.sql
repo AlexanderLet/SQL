b4d8289e8b13e3916319e3c1e36896fd31d41165
lesike <100831053+AlexanderLet@users.noreply.github.com>
2024-02-27 20:14:06 +0300

    Update 123.sql

--- 123.sql
+++ 123.sql
@@ -1,5 +1,12 @@
+//Вывести все операции по ApplePay, по которым не было возвратов.
 Select shop.*
 from shop
 JOIN pay ON shop.id = pay.shop_id
 where pay.status = 'REFUNDED'
 group by shop.id;
+
+
+//Вывести все магазины которые активны и URL содержит https. Должна быть сортировка по названию в обратном порядке.
+select * from shop
+where status = 'Activ' and url LIKE 'https%'
+order by name desc;
