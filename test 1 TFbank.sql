de2cc1b3d43cda494298010e7c80d61dcc984e79
lesike <100831053+AlexanderLet@users.noreply.github.com>
2024-02-27 20:24:56 +0300

    Update test 1 TFbank.sql

--- test 1 TFbank.sql
+++ test 1 TFbank.sql
@@ -1,4 +1,4 @@
-/*Тестовые данные таблицы*/
+/*Тестовые данные*/
 
 CREATE TABLE payments (
   id bigint PRIMARY KEY,       --ID операции
@@ -11,17 +11,17 @@ CREATE TABLE payments (
 
 
 INSERT INTO payments (id, status, amount, fee, shop_id, type_pay) VALUES
-(500, 'CONFIRMED', 10000, 3000, 211, 'ApplePay'),
-(501, 'REFUNDED', 540000, 20000, 211, 'GooglePay'),
-(502, 'CONFIRMED', 13281, 6934, 541, 'Card'),
-(503, 'CONFIRMED', 75608, 8687, 344, 'ApplePay'),
-(504, 'CONFIRMED', 53096, 3000, 781, 'GooglePay'),
-(505, 'REFUNDED', 22438, 4226, 234, 'Card'),
-(506, 'CONFIRMED', 74630, 3000, 189, 'ApplePay'),
-(507, 'CONFIRMED', 41299, 1020, 189, 'GooglePay'),
-(508, 'REFUNDED', 67691, 4795, 344, 'Card'),
-(509, 'REFUNDED', 39166, 3702, 211, 'ApplePay'),
-(510, 'CONFIRMED', 34126, 3442, 211, '');
+  (500, 'CONFIRMED', 10000, 3000, 211, 'ApplePay'),
+  (501, 'REFUNDED', 540000, 20000, 211, 'GooglePay'),
+  (502, 'CONFIRMED', 13281, 6934, 541, 'Card'),
+  (503, 'CONFIRMED', 75608, 8687, 344, 'ApplePay'),
+  (504, 'CONFIRMED', 53096, 3000, 781, 'GooglePay'),
+  (505, 'REFUNDED', 22438, 4226, 234, 'Card'),
+  (506, 'CONFIRMED', 74630, 3000, 189, 'ApplePay'),
+  (507, 'CONFIRMED', 41299, 1020, 189, 'GooglePay'),
+  (508, 'REFUNDED', 67691, 4795, 344, 'Card'),
+  (509, 'REFUNDED', 39166, 3702, 211, 'ApplePay'),
+  (510, 'CONFIRMED', 34126, 3442, 211, '');
 
 
 
@@ -34,18 +34,20 @@ CREATE TABLE shop (
 );
 
 INSERT INTO shop (id, name, created, status, url) VALUES
-(211, 'Aples', '01.01.2022', 'Activ', 'https://Aples.com'),
-(212, 'Pepe', '01.01.2022', 'Delete', 'http://Pepe.com'),
-(541, 'Mamma-mia', '15.11.2020', 'Activ', 'https://Mamma-mia.com'),
-(555, 'OOOPS', '01.01.1970', 'Activ', 'http://OOOPS.ru'),
-(344, 'YES', '21.05.2000', 'Activ', 'https://YES.ru'),
-(234, 'uzoo', '11.07.2021', 'Delete', 'https://uzoo.com'),
-(189, 'WOW', '18.09.2021', 'Activ', 'https://wow.com'),
-(200, 'HA-HA', '01.04.2021', 'Activ', 'https://HA-HA.com'),
-(201, 'he_he', '01.04.2021', 'Activ', 'http://he_he.com'),
-(777, 'VIP', '07.07.2007', 'Activ', 'https://VIP.vip');
+  (211, 'Aples', '01.01.2022', 'Activ', 'https://Aples.com'),
+  (212, 'Pepe', '01.01.2022', 'Delete', 'http://Pepe.com'),
+  (541, 'Mamma-mia', '15.11.2020', 'Activ', 'https://Mamma-mia.com'),
+  (555, 'OOOPS', '01.01.1970', 'Activ', 'http://OOOPS.ru'),
+  (344, 'YES', '21.05.2000', 'Activ', 'https://YES.ru'),
+  (234, 'uzoo', '11.07.2021', 'Delete', 'https://uzoo.com'),
+  (189, 'WOW', '18.09.2021', 'Activ', 'https://wow.com'),
+  (200, 'HA-HA', '01.04.2021', 'Activ', 'https://HA-HA.com'),
+  (201, 'he_he', '01.04.2021', 'Activ', 'http://he_he.com'),
+  (777, 'VIP', '07.07.2007', 'Activ', 'https://VIP.vip');
 
-/*Вывести все операции по ApplePay, по которым не было возвратов.*/
+
+/*Выполнить задания:
+Вывести все операции по ApplePay, по которым не было возвратов.*/
 Select shop.*
 from shop
 JOIN pay ON shop.id = pay.shop_id
